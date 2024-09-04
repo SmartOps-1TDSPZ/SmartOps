3137b10103cea0c03b225b6ed1e62301363369ba
Amanda Mesquita <amandamesquita73638@gmail.com>
2024-09-04 15:52:32 -0300

    Created Components Folder and Pages

---
+++ SmartOps/src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+const Footer: React.FC = () => {
+  return (
+    <S.FooterContainer>
+      <S.FooterText>© 2023 Todos os Direitos Reservados | OpsApp</S.FooterText>
+    </S.FooterContainer>
+  );
+};
+
+export default Footer;
