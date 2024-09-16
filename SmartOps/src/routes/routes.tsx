29e2ba213c985115e8879e081a176d2ddf06c108
Amanda Mesquita <amandamesquita73638@gmail.com>
2024-09-16 20:34:21 -0300

    Updated Section Blue e Routes

--- SmartOps/src/routes/routes.tsx
+++ SmartOps/src/routes/routes.tsx
@@ -1,10 +1,23 @@
 import Home from "../pages/home";
+import Login from "../pages/login";
+import Members from "../pages/members";
+import Register from "../pages/register";
 
 export const routes = [
   {
     path: "/",
     element: <Home />,
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/members",
+    element: <Members />,
+  },
 ];
-
-export default routes;
