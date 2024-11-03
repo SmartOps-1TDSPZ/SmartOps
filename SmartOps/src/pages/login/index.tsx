f196c45b9060c2d693cd8c58a1ce1a37a9d7b10d
Amanda Mesquita <amandamesquita73638@gmail.com>
2024-11-03 19:15:56 -0300

    Updated login page

--- SmartOps/src/pages/login/index.tsx
+++ SmartOps/src/pages/login/index.tsx
@@ -18,9 +18,7 @@ const Login: React.FC = () => {
     try {
       const result = await loginUser(email, password);
       console.log("Login realizado com sucesso:", result);
-    } catch (error) {
-      console.error("Erro no login:", error);
-    }
+    
   };
 
   return (
