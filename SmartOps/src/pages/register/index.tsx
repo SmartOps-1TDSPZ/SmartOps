4c52f0708226890d1a457926f048a0dd8c403ec6
Amanda Mesquita <amandamesquita73638@gmail.com>
2024-11-04 18:59:46 -0300

    Updated Register page

--- SmartOps/src/pages/register/index.tsx
+++ SmartOps/src/pages/register/index.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import "./styles.css";
-
-import { Link } from "react-router-dom";
-import Car from "../../assets/car.svg";
-import Ellipse from "../../assets/Ellipse 5.svg";
-import Logo from "../../assets/Logo-white-LadingPage.svg";
 import { registerUser } from "../../services/apiService";
 
 const Register: React.FC = () => {
@@ -15,9 +9,19 @@ const Register: React.FC = () => {
     const email = (form.elements.namedItem("email") as HTMLInputElement).value;
     const password = (form.elements.namedItem("password") as HTMLInputElement)
       .value;
+    const numero = (form.elements.namedItem("numero") as HTMLInputElement)
+      .value;
+    const endereco = (form.elements.namedItem("endereco") as HTMLInputElement)
+      .value;
 
     try {
-      const result = await registerUser(name, email, password);
+      const result = await registerUser(
+        name,
+        email,
+        password,
+        numero,
+        endereco
+      );
       console.log("Cadastro realizado com sucesso:", result);
     } catch (error) {
       console.error("Erro no cadastro:", error);
@@ -25,61 +29,26 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className="allign-all">
-      <header>
-        <img src={Ellipse} alt="Google logo" className="blue-circle" />
-        <img src={Logo} alt="Logo Escrito SmartOps" className="smart-ops" />
-        <img src={Car} alt="Imagem de um carro" className="car" />
-      </header>
-
-      <div className="container">
-        <section
-          className="register-box"
-          role="form"
-          aria-labelledby="register-title"
-        >
-          <h2 className="register-title">Cadastro</h2>
-          <form onSubmit={handleRegisterSubmit}>
-            <div className="input-group">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                required
-                placeholder="Informe seu nome"
-                aria-required="true"
-              />
-            </div>
-            <div className="input-group">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                placeholder="Informe seu email"
-                aria-required="true"
-              />
-            </div>
-            <div className="input-group">
-              <input
-                type="password"
-                id="password"
-                name="password"
-                required
-                placeholder="Informe sua senha"
-                aria-required="true"
-              />
-            </div>
-            <button type="submit" className="btn-continue">
-              Continuar
-            </button>
-          </form>
-          <p className="links">
-            Já tem cadastro?
-            <Link to="/login">Entre agora!</Link>
-          </p>
-        </section>
-      </div>
+    <div className="container">
+      <h2>Registro</h2>
+      <form onSubmit={handleRegisterSubmit}>
+        <div className="input-group">
+          <input type="text" name="name" required placeholder="Nome" />
+        </div>
+        <div className="input-group">
+          <input type="email" name="email" required placeholder="E-mail" />
+        </div>
+        <div className="input-group">
+          <input type="password" name="password" required placeholder="Senha" />
+        </div>
+        <div className="input-group">
+          <input type="text" name="numero" required placeholder="Número" />
+        </div>
+        <div className="input-group">
+          <input type="text" name="endereco" required placeholder="Endereço" />
+        </div>
+        <button type="submit">Cadastrar</button>
+      </form>
     </div>
   );
 };
